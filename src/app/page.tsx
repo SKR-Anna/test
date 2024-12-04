b35fe19cba42e310e2d1952afa2da7a4bc308d7e
SKR-Anna <anutich19@gmail.com>
2024-12-04 20:23:23 +0300

    исправила запрос, получила массив

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import { notFound } from "next/navigation";
 
 const fetchData = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com', {
-    cache: 'force-cache',
+  const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+    // cache: 'force-cache',
     next: {
       revalidate: 3600 // 1 hour
     },
