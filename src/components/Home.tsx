342dc37074dca67e5f654edac0a528c0ed34c8df
SKR-Anna <anutich19@gmail.com>
2024-12-05 18:20:42 +0300

    сделала карточку на домашней странице

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,27 +10,27 @@ import Link from "next/link";
 import { User } from '@/types/types';
 
 interface Props {
-    users: User[]
+    user: User
 };
 
-const Card = ({ users }: Props) => {
+const UserCard: React.FC<Props> = ({ user }) => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4"></div>
-    )
-}
-
-<Card>
+    <Card>
     <CardHeader>
-        <CardTitle>user name</CardTitle>
+        <CardTitle>{user.name}</CardTitle>
     </CardHeader>
     <CardContent>
-        <p>email</p>
-        <p>company</p>
+        <p>{user.email}</p>
+        <p>{user.company.name}</p>
     </CardContent>
     <CardFooter>
-        <Link href={}> переход на карточку person
+        <Link href={`/user/${user.id}`}> переход на карточку person
         </Link>
     </CardFooter>
 </Card>
+    )
+}
 
-export default <Card/>;
+
+
+export default UserCard;
