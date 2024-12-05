75365861f3327f7215cbcef8d5ecff814703e393
SKR-Anna <anutich19@gmail.com>
2024-12-05 18:46:10 +0300

    сделала page главной страницы

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,56 @@
-import { notFound } from "next/navigation";
+import { GetServerSideProps } from "next";
+import { User } from "../types/types";
+import UserCard from "@/components/Home";
 
-const fetchData = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users', {
-    // cache: 'force-cache',
-    next: {
-      revalidate: 3600 // 1 hour
-    },
-  });
-  const data = await response.json();
-  return data
+interface Props {
+  users: User[];
 }
 
-export default async function Page() {
-  const data = await fetchData();
-  console.log(data);
-  if(!data) notFound();
+const Page = ({ users}: Props) => {
+  return(
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+      {users.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  try{
+    const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+      next: {
+        revalidate: 3600 // 1 hour
+  }
+});
+    const users: User[] = await response.json();
+    return {props: {users} };
+  } catch (error) {
+    console.error('Ошибка при получении данных:', error)
+    return {props: {users:[]} } // Возвращаем пустой массив в случае ошибки
+  }
+}
+
+export default Page;
+
+
+
+// const fetchData = async () => {
+//   const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+//     // cache: 'force-cache',
+//     next: {
+//       revalidate: 3600 // 1 hour
+//     },
+//   });
+//   const data = await response.json();
+//   return data
+// }
+
+// export default async function Page() {
+//   const data = await fetchData();
+//   console.log(data);
+//   if(!data) notFound();
   
-  return (
-  <div> Стартовая страница </div>
-)};
+//   return (
+//   <div> Стартовая страница </div>
+// )};
