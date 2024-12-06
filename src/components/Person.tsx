4f4a33105bb83eb7571dd03bf9c72b5d6dd391d8
SKR-Anna <anutich19@gmail.com>
2024-12-06 22:18:28 +0300

    добавила стили персональной карточке

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -15,19 +15,19 @@ interface Props {
 
 const PersonCard: React.FC<Props> = ({ user }) => {
     return (
-        <Card>
-            <CardHeader>
-                <CardTitle>{user.name}</CardTitle>
-                <CardDescription>{user.username}</CardDescription>
+        <Card className="bg-white shadow-lg rounded-lg overflow-hidden">
+            <CardHeader className="p-4 border-b">
+                <CardTitle className="text-xl font-semibold text-gray-800">{user.name}</CardTitle>
+                <CardDescription className="text-gray-500">{user.username}</CardDescription>
             </CardHeader>
-            <CardContent>
-                <p>{user.email}</p>
-                <p>{user.website}</p>
-                <p>{user.company.name}</p>
+            <CardContent className="p-4">
+                <p className="text-gray-700">{user.email}</p>
+                <p className="text-gray-700">{user.website}</p>
+                <p className="text-gray-700">{user.company.name}</p>
             </CardContent>
-            <CardFooter>
-                <p>{`${user.address.street} , ${user.address.city}` }</p>
-                <p>{user.phone}</p>
+            <CardFooter className="p-4 border-t">
+                <p className="text-gray-600">{`${user.address.street} , ${user.address.city}` }</p>
+                <p className="text-gray-600">{user.phone}</p>
             </CardFooter>
         </Card>
     )
